0b475585facf9847ecdf9485502f85a0567ee6e6
lexiiyz <radityarakha01@gmail.com>
2026-03-01 13:58:30 +0700

    fix prisma error

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,12 @@
+import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client/client";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
 const prismaClientSingleton = () => {
-  const adapter = new PrismaPg({ connectionString });
+  const pool = new Pool({ connectionString });
+  const adapter = new PrismaPg(pool);
   return new PrismaClient({ adapter });
 };
 
